37968abf791aba3d11f2e8b7ca33312b6cce6f71
marcelotaguiar <marcelo.taguiar@senacsp.edu.br>
2026-09-03 09:38:22 -0300

    Create view.sql
    Create view vw_monitoramento_sarampo for data analysis

---
+++ sql/sql/04_views.sql
@@ -0,0 +1,29 @@
+USE pi_sarampo;
+
+CREATE VIEW vw_monitoramento_sarampo AS
+SELECT
+    m.nome_municipio,
+    m.estado,
+    n.ano_notificacao,
+    n.numero_casos,
+    c.cobertura_d1,
+    c.cobertura_d2,
+    c.atingiu_meta,
+    COALESCE(SUM(i.quantidade_imigrantes), 0) AS total_imigrantes
+FROM municipios m
+LEFT JOIN notificacoes n
+    ON m.id_municipio = n.id_municipio
+LEFT JOIN cobertura_vacinal c
+    ON m.id_municipio = c.id_municipio
+    AND n.ano_notificacao = c.ano_cobertura
+LEFT JOIN imigracao i
+    ON m.id_municipio = i.id_municipio
+    AND n.ano_notificacao = i.ano_imigracao
+GROUP BY
+    m.nome_municipio,
+    m.estado,
+    n.ano_notificacao,
+    n.numero_casos,
+    c.cobertura_d1,
+    c.cobertura_d2,
+    c.atingiu_meta;
